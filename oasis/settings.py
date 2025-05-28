70a8d5ba300ef4698994fb9e74fb9f81f5296c40
Aniekan Essiet <essiet.aniekan@yahoo.com>
2025-05-29 02:29:26 +0300

    fixed

--- oasis/settings.py
+++ oasis/settings.py
@@ -26,8 +26,10 @@ SECRET_KEY = 'django-insecure-%^!yx4t!4_f@eit$1@u_a!^ltsoy5r0x&ow$_e)@s$h(w&*cqt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1:8000', '*']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app','now.sh', 'localhost' 'oasispetroleumllc.com', 'www.oasispetroleumllc.com']
 
+#Site ID
+SITE_ID = 1
 
 # Application definition
 
@@ -38,6 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'django.contrib.sitemaps',
+    'django.contrib.sites',
+
     'petroleum',
     'fontawesomefree',
 ]
@@ -84,6 +90,17 @@ DATABASES = {
     }
 }
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'railway',
+#        'USER': 'postgres',
+#        'PASSWORD': 'dAFD142DdbfE*F2FCc--A5*-a5feg*af',
+#        'HOST': 'monorail.proxy.rlwy.net',
+#        'PORT': '51627',
+#    }
+#}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -120,16 +137,29 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/oasis/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+#STATIC_ROOT = '/oasis/static/'
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#MEDIA_URL = '/media/'
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+#STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
+
+# Vercel Deployment settings
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Email Settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_HOST_USER = 'Chrispishon@gmail.com'
+EMAIL_HOST_PASSWORD = 'jtfiwelvytwxsral'
+EMAIL_USE_TLS = True
